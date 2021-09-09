6c801567ed8ddba0dca5a33f278ce2972623d69f
Denis <lose.den16@gmail.com>
2021-09-09 13:16:22 +0300

    Add NickName set

--- Battle Royale/Assets/_Scripts/ListItem.cs
+++ Battle Royale/Assets/_Scripts/ListItem.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 using TMPro;
 using Photon.Realtime;
+using Photon.Pun;
 
 public class ListItem : MonoBehaviour
 {
     [SerializeField] TMP_Text roomName;
     [SerializeField] TMP_Text playerCount;
 
+
     public void SetInfo(RoomInfo info)
     {
         roomName.text = info.Name;
         playerCount.text = info.PlayerCount + "/" + info.MaxPlayers;
+    }
+
+    public void JoinToListRoom()
+    {
+        PhotonManager.Instance.SetNickName();
+        PhotonNetwork.JoinRoom(roomName.text);
     }
 }
--- Battle Royale/Assets/_Scripts/PhotonManager.cs
+++ Battle Royale/Assets/_Scripts/PhotonManager.cs
@@ -8,21 +8,35 @@ using TMPro;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
+    public static PhotonManager Instance;
+
     [SerializeField] private string region;
     [SerializeField] TMP_InputField RoomName;
     [Space]
     [SerializeField] ListItem itemPrefab;
     [SerializeField] Transform content;
+    [Space]
+    [SerializeField] TMP_InputField nickName;
+
+    List<RoomInfo> allRoomsInfo = new List<RoomInfo>();
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
         PhotonNetwork.ConnectToRegion(region);
-    }    
+    }
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     public override void OnConnectedToMaster()
     {
         Debug.Log("Вы подключились к: " + PhotonNetwork.CloudRegion);
-        PhotonNetwork.JoinLobby();
+        
+        if(!PhotonNetwork.InLobby)
+            PhotonNetwork.JoinLobby();
+
+        
     }
 
     public override void OnDisconnected(DisconnectCause cause)
@@ -40,11 +54,13 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 10;
         PhotonNetwork.CreateRoom(RoomName.text, roomOptions, TypedLobby.Default);
+        SetNickName();
         PhotonNetwork.LoadLevel("Game");
     }
 
     public override void OnCreatedRoom()
     {
+        
         Debug.Log("Создана комната, имя комнаты: " + PhotonNetwork.CurrentRoom.Name);
     }
 
@@ -57,9 +73,57 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     {
         foreach (RoomInfo info in roomList)
         {
+            for (int i = 0; i < allRoomsInfo.Count; i++)
+            {
+                if (allRoomsInfo[i].masterClientId == info.masterClientId)
+                    return;
+            }
+
             ListItem listItem = Instantiate(itemPrefab, content);
-            if (listItem != null)            
-                listItem.SetInfo(info);            
+            if (listItem != null)
+            {
+                listItem.SetInfo(info);
+                allRoomsInfo.Add(info);
+            }                                
         }
+    }
+
+    public override void OnJoinedRoom()
+    {        
+        Debug.Log("Ваш nick: " + PhotonNetwork.NickName);
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    public void JoinRandomRoomButton()
+    {
+        SetNickName();
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void JoinButton()
+    {
+        SetNickName();
+        PhotonNetwork.JoinRoom(RoomName.text);
+    }
+
+    public void LeaveButton()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.LoadLevel("MainScene");
+    }
+
+    public void SetNickName()
+    {
+        string PlayerNickName = nickName.text;
+        if (PlayerNickName == "")
+        {
+            PhotonNetwork.NickName = "User";
+        }
+        else
+            PhotonNetwork.NickName = PlayerNickName;
     }
 }
